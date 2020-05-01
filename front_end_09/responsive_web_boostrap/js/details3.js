5c9a250a6d9d69b757517a173615eb5bd72b00c6
velocity5 <40029695+velocity5@users.noreply.github.com>
2020-05-01 21:20:47 +0700

    fix home menu

---
+++ front_end_09/responsive_web_boostrap/js/details3.js
@@ -0,0 +1,47 @@
+/*responsive click*/
+$(document).ready(function() {
+	
+	$('.a0').click(function() {
+		$('html, body').animate({scrollTop:$('#product').offset().top});
+		return false;
+
+	})
+	$('.a1').click(function() {
+		$('html, body').animate({scrollTop:$('#about').offset().top});
+		return false;
+	})
+	$('.a2').click(function() {
+		$('html, body').animate({scrollTop:$('#service').offset().top});
+		return false;
+	})
+	$('.a3').click(function() {
+		$('html, body').animate({scrollTop:$('#footer').offset().top});
+		return false;
+	})
+	$('.find').click(function() {
+		$('html, body').stop(true, true).delay(300).animate({scrollTop:$('#product').offset().top}, "slow");
+		return false;
+	})
+})
+
+/*Scroll to top when arrow up clicked*/
+$(window).scroll(function() {
+    var height = $(window).scrollTop();
+    if (height > 100) {
+        $('#back2Top').fadeIn();
+    } else {
+        $('#back2Top').fadeOut();
+    }
+});
+$(document).ready(function() {
+    $("#back2Top").click(function(event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
+
+});
+/* click menu ra */
+$(document).on('click', function() {
+	$('.collapse').collapse('hide');
+});
