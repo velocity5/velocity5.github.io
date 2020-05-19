aa32d19f6b9d7f3676fb72a59aeb1dbe0f978dc1
velocity5 <40029695+velocity5@users.noreply.github.com>
2020-05-19 14:59:54 +0700

    md effect click btn

--- front_end_09/dom/calculator_prj/script.js
+++ front_end_09/dom/calculator_prj/script.js
@@ -1,84 +1,74 @@
-let val = "", number, decimal, equal, operator, allowFactorial = true, allowSquareRoot = true;
+let currentVal = "", number, decimal, equal, operator;
 let display = document.forms['myForm']['display'];
 
 function getNumber(num) {
 	if (equal) {
-		val = num;
+		currentVal = num;
 		display.value = val;
 		number = true;
 		equal = false;
 	}
 	else {
-		val = display.value + num;
-		display.value = val;
+		currentVal = display.value + num;
+		display.value = currentVal;
 		number = true;
 	}
 	if (operator) {
 		decimal = false;
 	}
-	squareRoot('a');
-	
 }
 
 function getOperand(op) {
-	display.value = val + op;
+	display.value = currentVal + op;
 	operator = true;
 	equal = false;
-	allowSquareRoot = false;
-	allowFactorial = false;
-	squareRoot('a');
-	
 }
 function getDecimal() {
 	if (number && !decimal) {
-		display.value = val + '.';
+		display.value = currentVal + '.';
 		decimal = true;
 		operator = false;
 	}
 }
 
 function compute() {
-	if (val) {
-		val = eval(val);
-		display.value = val;
+	if (currentVal) {
+		currentVal = eval(currentVal);
+		display.value = currentVal;
 		equal = true;
 		decimal = false;
 		number = false;
-		allowSquareRoot = true;
-		allowFactorial = true;
-		squareRoot('a');
-		
 	}
 }
 
 function allClear() {
-	val = '';
-	display.value = val;
+	currentVal = '';
+	display.value = currentVal;
 	decimal = false;
 }
 
 function backSpace() {
-	val = display.value;
-	val = val.substr(0, val.length - 1);
-	display.value = val;
+	currentVal = display.value;
+	currentVal = currentVal.substr(0, currentVal.length - 1);
+	display.value = currentVal;
 }
 
 function squareRoot(x) {
 	if (x == 'r') {
-		val = Math.sqrt(val);
-		display.value = val;
+		currentVal = Math.sqrt(currentVal);
+		display.value = currentVal;
 	}
 	else if (x == 's') {
-		
-		val = val * val;
-		display.value = val;
+		currentVal = currentVal * currentVal;
+		display.value = currentVal;
 	}
-	else if (x == 'a' && allowSquareRoot) {
-		document.getElementById('r').disabled = false;
-		document.getElementById('s').disabled = false;
-	}
-	else if (!allowSquareRoot) {
-		document.getElementById('r').disabled = true;
-		document.getElementById('s').disabled = true;
+}
+function getFactorial() {
+	if (currentVal) {
+		let result = 1;
+		for (let i = 1; i <= currentVal; i++) {
+			result = result * i;
+		}
+		display.value = result;
 	}
 }
