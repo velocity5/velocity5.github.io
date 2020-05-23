701144fae26a957fce2580b24fb02f99d5fcbb34
velocity5 <40029695+velocity5@users.noreply.github.com>
2020-05-23 15:54:16 +0700

    cal jquery

--- front_end_09/dom/calculator_prj/calculator_jquery/index.js
+++ front_end_09/dom/calculator_prj/calculator_jquery/index.js
@@ -1,132 +1,106 @@
-// keep track value
-const calculator = {
-	displayValue: '0',
-	firstOperand: null,
-	pendingOperand: false,
-	operator: null
-};
-// update display
-updateDisplay = () => {
-	const display = document.getElementById('display');
-	display.value = calculator.displayValue;
-}
-updateDisplay();
-// key presses
-const keys = document.getElementById('key');
-keys.addEventListener('click', (e) => {
-	const { target } = e;
-	if (!target.matches('input')) {
-		return;
-	}
-	if (target.classList.contains('operand')) {
-		getOperator(target.value);
-		updateDisplay();
-		return;
-	}
-	if (target.classList.contains('decimal')) {
-		inputDecimal(target.value);
-		updateDisplay();
-		return;
-	}
-	if (target.classList.contains('all-clear')) {
-		clearAll();
-		updateDisplay();
-		return;
-	}
-	if (target.classList.contains('backspace')) {
-		backSpace();
-		updateDisplay();
-		return;
-	}
-	inputDigit(target.value);
-	updateDisplay();
-})
-// input digits
-inputDigit = (digit) => {
-	const { displayValue, pendingOperand } = calculator;
-	if (pendingOperand == true) {
-		calculator.displayValue = digit;
-		calculator.pendingOperand = false;
-	} else {
-		calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
-	}
-	console.log(calculator);
-}
-// input decimal
-inputDecimal = (dot) => {
-	if (calculator.pendingOperand == true) return;
-	if (!calculator.displayValue.includes(dot)) {
-		calculator.displayValue += dot;
-	}
-}
-// operator
-getOperator = (nextOperator) => {
-	const { firstOperand, displayValue, operator } = calculator;
-	const inputValue = parseFloat(displayValue);
-	if (operator && calculator.pendingOperand) {
-		calculator.operator = nextOperator;
-		console.log(operator);
-		return;
-	}
+let currentVal = "", number, decimal, equal, operator;
+let display = document.forms['myForm']['display'];
+// let ct;
+$(document).ready(function () {
+	$(".number").click(function () {
+		// if (!operator) { $("#display").val("") }
+		Val = $(this).val(); /* get number and assign to currentVal*/
+		console.log(Val);
+		dp = $("#display").val();/* get display value */
+		if (dp) {
+			$("#display").val(dp + Val);
+		}
+		else {
+			$("#display").val(Val);
+		}
 
-	if (firstOperand == null) {
-		calculator.firstOperand = inputValue;
-	} else if (operator) {
-		const result = performCalculation[operator](firstOperand, inputValue);
-		calculator.displayValue = String(result);
-		calculator.firstOperand = result;
+		if (dp) { currentVal = dp + Val; }
+		else { currentVal = Val; }
+	});
+
+	$("#decimal").click(function () {
+		$("#display").val(currentVal + '.');
+		decimal = true;
+		operator = false;
+	})
+	$("#percent").click(function () {
+		$("#display").val(currentVal / 100);
+	})
+
+	$("#allClear").click(function () {
+		$("#display").val('');
+	})
+	$("#backSpace").click(function () {
+		$("#display").val($("#display").val()
+			.substr(0, $("#display").val().length - 1));
+	})
+	$("#equal").click(function () {
+		if (operator === "x") {
+			result = eval($("#display").val().replace("x", "*"));
+		}
+		else if (operator === "/") {
+			result = eval($("#display").val().replace("÷", "/"));
+		}
+		else {
+			result = eval($("#display").val());
+		}
+		currentVal = result;
+		$("#display").val(result);
+		operator = false;
+	})
+
+});
+
+function getOperand(op) {
+	display.value = currentVal + op;
+	if (op === "*") {
+		display.value = currentVal + op.replace("*", "x");
+		return operator = "x";
 	}
-	calculator.pendingOperand = true;
-	calculator.operator = nextOperator;
-	console.log(calculator);
+	else if (op === "/") {
+		display.value = currentVal + op.replace("/", "÷");
+		return operator = "/";
+	}
+	operator = true;
+	equal = false;
 }
 
-const performCalculation = {
-	'+': (firstOperand, pendingOperand) => firstOperand + pendingOperand,
-	'-': (firstOperand, pendingOperand) => firstOperand - pendingOperand,
-	'÷': (firstOperand, pendingOperand) => firstOperand / pendingOperand,
-	'x': (firstOperand, pendingOperand) => firstOperand * pendingOperand,
-	'%': (firstOperand, pendingOperand) => firstOperand % pendingOperand,
-	'=': (firstOperand, pendingOperand) => pendingOperand
-};
-// reset calculator
-clearAll = () => {
-	calculator.displayValue = '0';
-	calculator.firstOperand = null;
-	calculator.pendingOperand = false;
-	calculator.operator = null;
-	console.log(calculator);
-}
-// backspace
-backSpace = () => {
-	let value = calculator.displayValue.slice(0, - 1);
-	calculator.displayValue = value;
-};
-// function Math
-const functionCal = (e) => {
-	let currentNumb = Number(firstOperand.val());
-	switch (e) {
-		case 'x':
-			console.log(Number, e);
-			return currentNumb * currentNumb;
-		case '&radic;':
-			return Math.sqrt(currentNumb);
-		case 'sin':
-			return Math.sin(currentNumb);
-		case 'cos':
-			return Math.cos(currentNumb);
-		case 'x!':
-			return calFactorial(currentNumb);
+
+function squareRoot(x) {
+	if (x == 'root') {
+		currentVal = Math.sqrt(currentVal);
+		display.value = currentVal;
+	}
+	else if (x == 'square') {
+		currentVal = currentVal * currentVal;
+		display.value = currentVal;
 	}
 }
-
+function sincosCal(x) {
+	if (x == 'sin') {
+		currentVal = Math.sin(currentVal);
+		display.value = currentVal;
+	}
+	else if (x == 'cos') {
+		currentVal = Math.cos(currentVal);
+		display.value = currentVal;
+	}
+}
+function getFactorial() {
+	if (currentVal) {
+		let result = 1;
+		for (let i = 1; i <= currentVal; i++) {
+			result = result * i;
+		}
+		display.value = result;
+	}
+}
 // change background color
-const button = document.getElementById("color");
-const body = document.querySelector("body");
-const colors = ["orange", "gold", "violet", "cyan"];
-body.style.backgroundColor = "gold";
-button.addEventListener('click', changeColor);
-function changeColor() {
-	const colorId = parseInt(Math.random() * colors.length);
-	body.style.backgroundColor = colors[colorId];
-}
+const button = document.getElementsByClassName('btn-grad');
+const body = document.querySelector('body');
+const colors = ['crimson', 'orange', 'green', 'cyan'];
+body.style.backgroundColor = 'violet';
+$(button).click(function () {
+	const colorIndex = parseInt(Math.random() * colors.length);
+	body.style.backgroundColor = colors[colorIndex];
+})
