ddade04b80d179d857f3b16f2851404c4f9c117c
velocity5 <40029695+velocity5@users.noreply.github.com>
2020-05-27 23:32:14 +0700

    viet hoa

--- front_end_09/form/signup/index.js
+++ front_end_09/form/signup/index.js
@@ -23,18 +23,18 @@ $(document).ready(function () {
         let pwdVal = $('#psw').val();
         let nameVal = $('#inputName').val();
         if (emailAdd == '' || pwdVal == '' || nameVal == '') {
-            $('#inputEmail').after(`<span class='error' style='color:crimson;background:none'><i class="fas fa-exclamation-triangle" style="background:none"></i> Please enter your email address.</span>`);
-            $('#psw').after(`<span class='error' style='color:crimson;background:none'><i class="fas fa-exclamation-triangle" style="background:none"></i> Please enter your password.</span>`);
-            $('#inputName').after(`<span class='error' style='color:crimson;background:none'><i class="fas fa-exclamation-triangle" style="background:none"></i> Please enter your name.</span>`);
+            $('#inputEmail').after(`<span class='error' style='color:crimson;background:none'><i class="fas fa-exclamation-triangle" style="background:none"></i> Email không được bỏ trống.</span>`);
+            $('#psw').after(`<span class='error' style='color:crimson;background:none'><i class="fas fa-exclamation-triangle" style="background:none"></i> Mật khẩu không được bỏ trống.</span>`);
+            $('#inputName').after(`<span class='error' style='color:crimson;background:none'><i class="fas fa-exclamation-triangle" style="background:none"></i> Họ tên không được bỏ trống.</span>`);
             hasError = true;
         } else if (!nameReg.test(nameVal)) {
-            $('#inputName').after(`<span class='error' style='color:crimson;background:none'><i class="fas fa-exclamation-triangle" style="background:none"></i> Please enter a text name only.</span>`);
+            $('#inputName').after(`<span class='error' style='color:crimson;background:none'><i class="fas fa-exclamation-triangle" style="background:none"></i> Tên chỉ được nhập chữ.</span>`);
             hasError = true;
         } else if (!emailReg.test(emailAdd)) {
-            $('#inputEmail').after(`<span class='error' style='color:crimson;background:none'><i class="fas fa-exclamation-triangle" style="background:none"></i> Please enter a valid email.</span>`);
+            $('#inputEmail').after(`<span class='error' style='color:crimson;background:none'><i class="fas fa-exclamation-triangle" style="background:none"></i> Email không hợp lệ.</span>`);
             hasError = true;
         } else if (!passReg.test(pwdVal)) {
-            $('#psw').after(`<span class='error' style='color:crimson;background:none'><i class="fas fa-exclamation-triangle" style="background:none"></i> Password must contain at least 8 chars, 1 number, 1 uppercase letter A-Z, 1 lowercase letter a-z.</span>`);
+            $('#psw').after(`<span class='error' style='color:crimson;background:none'><i class="fas fa-exclamation-triangle" style="background:none"></i> Mật khẩu phải chứa ít nhất 8 ký tự, 1 số, 1 chữ hoa A-Z, 1 chữ thường a-z.</span>`);
             hasError = true;
         }
         if (hasError == true) { return false; }
