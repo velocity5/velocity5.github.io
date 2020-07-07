07e1101304826976049746c2dbbdb66525e50b36
velocity5 <40029695+velocity5@users.noreply.github.com>
2020-07-07 22:08:01 +0700

    zigtoys pj

---
+++ front_end_09/zigtoys/js/details2.js
@@ -0,0 +1,49 @@
+// Carousel Auto-Cycle
+  $(document).ready(function() {
+    $('.carousel').carousel({
+      interval: 6000
+    })
+  });
+/*responsive click*/
+$(document).ready(function() {
+	
+	$('.click-about').click(function() {
+		$('html, body').animate({scrollTop:$('#about').offset().top}, "slow");
+		return false;
+
+	})
+	$('.click-product').click(function() {
+		$('html, body').animate({scrollTop:$('#product').offset().top}, "slow");
+		return false;
+	})
+	$('.click-service').click(function() {
+		$('html, body').animate({scrollTop:$('#service').offset().top}, "slow");
+		return false;
+	})
+	$('.click-contact').click(function() {
+		$('html, body').animate({scrollTop:$('#contact').offset().top}, "slow");
+		return false;
+	})
+})
+
+/*Scroll to top when arrow up clicked*/
+$(window).scroll(function() {
+    var height = $(window).scrollTop();
+    if (height > 100) {
+        $('#back2Top').fadeIn();
+    } else {
+        $('#back2Top').fadeOut();
+    }
+});
+$(document).ready(function() {
+    $("#back2Top").click(function(event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
+
+});
+/* click menu ra */
+$(document).on('click', function() {
+	$('.collapse').collapse('hide');
+});
---
+++ front_end_09/zigtoys/js/index.js
@@ -0,0 +1,137 @@
+﻿
+/*responsive click*/
+
+// scroll spy
+
+/* function scroll */
+
+/* responsive on mobile */
+
+/* click menu out */
+$(document).on('click', function() {
+	$('.collapse').collapse('hide');
+});
+/*Scroll to top when arrow up clicked*/
+$(window).scroll(function() {
+    var height = $(window).scrollTop();
+    if (height > 100) {
+        $('#back2Top').fadeIn();
+    } else {
+        $('#back2Top').fadeOut();
+    }
+});
+$(document).ready(function() {
+    $("#back2Top").click(function(event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
+
+});
+
+ /*Scroll to top when arrow up clicked END*/
+ /*validate email*/
+
+ $(document).ready(function() {
+ 	$('#inputEmail').change(function() {
+ 		let sEmail = $('#inputEmail').val();
+ 		let filter = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+ 		if (!filter.test(sEmail)) {
+ 			$("#error_email").text(sEmail+" is not valid email");
+ 			sEmail.focus;
+ 		} else {
+ 			$("#error_email").text("");
+ 		}
+ 	});
+ 	$("#btn-login").click(function(e) {
+ 		let focusSet = false;
+ 		let sEmail = $('#inputEmail').val();
+ 		let filter = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+ 		if (!sEmail) {
+ 		if ($("#inputEmail").parent().next(".validation").length == '') //only add if nothing input
+ 			{
+ 				$("#inputEmail").parent().after("<div class='validation' style='color:red;margin-bottom: 20px;'>Please enter email address</div>");
+ 			} 
+ 			e.preventDefault();
+ 			$("#inputEmail").focus();
+ 			focusSet = true;
+ 		} else {
+ 			$("#inputEmail").parent().next(".validation").remove();
+ 		}
+ 		let psw = $('#inputPassword').val();
+ 		if (!psw) {
+ 			if ($("#inputPassword").parent().next(".validation").length == '') //only add if nothing input
+ 			{
+ 				$("#inputPassword").parent().after("<div class='validation' style='color:red;margin-bottom: 20px;'>Please enter password</div>");
+ 			} 
+ 			e.preventDefault();
+ 			if (!focusSet) {
+ 				$("#inputPassword").focus();
+ 			}
+ 		} else {
+ 			$("#inputPassword").parent().next(".validation").remove();
+ 		}
+ 	});
+ });
+
+ //validate password
+ $(document).ready(function() {
+ 	$('input[type=password]').keyup(function() {
+ 		let psw = $(this).val();
+ 		//validate the length
+ 		if (psw.length < 8) {
+ 			$('#length').removeClass('valid').addClass('invalid');
+ 		} else {
+ 			$('#length').removeClass('invalid').addClass('valid');
+ 		}
+ 		//validate letter
+ 		if (psw.match(/[A-z]/)) {
+ 			$('#letter').removeClass('invalid').addClass('valid');
+ 		} else {
+ 			$('#letter').removeClass('valid').addClass('invalid');
+ 		}
+ 		//validate capital
+ 		if (psw.match(/[A-Z]/)) {
+ 			$('#capital').removeClass('invalid').addClass('valid');
+ 		} else {
+ 			$('#capital').removeClass('valid').addClass('invalid');
+ 		}
+ 		//validate number
+ 		if (psw.match(/\d/)) {
+ 			$('#number').removeClass('invalid').addClass('valid');
+ 		} else {
+ 			$('#number').removeClass('valid').addClass('invalid');
+ 		}
+ 	}).focus(function() {
+ 		$('#psw-info').show();
+ 	}).blur(function() {
+ 		$('#psw-info').hide();
+ 	})
+ });
+ // show-hide password
+$('.glyphicon-eye-open').on('click', 
+	function() {
+	$(this).toggleClass("glyphicon-eye-close");
+	let type = $("#inputPassword").attr("type");
+	if (type == "text") {
+		$('#inputPassword').attr("type", "password");
+	} else {
+		$('#inputPassword').attr("type", "text");
+	}
+});
+
+// click menu ra
+/*$(".menu-btn").click(function() {
+	$(".menu-left").addClass("out");
+	return false;
+})*/
+// click menu vao
+/*$(".close-btn").click(function() {
+	$(".menu-left").removeClass("out");
+	return false;
+})
+$("body").click(function() {
+	$(".menu-left").removeClass("out");
+	return false;
+})*/
+
