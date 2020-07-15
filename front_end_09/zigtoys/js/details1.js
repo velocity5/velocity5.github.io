ad73ebb20457b229bcd3cd1dab31e3c873d5908d
velocity5 <40029695+velocity5@users.noreply.github.com>
2020-07-15 23:07:45 +0700

    product detail

--- front_end_09/zigtoys/js/details1.js
+++ front_end_09/zigtoys/js/details1.js
@@ -1,58 +1,12 @@
-/*responsive click*/
-$(document).ready(function() {
-    
-    $('.click-about').click(function() {
-        $('html, body').animate({scrollTop:$('#about').offset().top}, "slow");
-        return false;
+let increBtn = document.querySelector(".qty-incre");
+let descBtn = document.querySelector(".qty-desc");
+let input = document.querySelector("#qty");
 
-    })
-    $('.click-product').click(function() {
-        $('html, body').animate({scrollTop:$('#product').offset().top}, "slow");
-        return false;
-    })
-    $('.click-service').click(function() {
-        $('html, body').animate({scrollTop:$('#service').offset().top}, "slow");
-        return false;
-    })
-    $('.click-contact').click(function() {
-        $('html, body').animate({scrollTop:$('#contact').offset().top}, "slow");
-        return false;
-    })
+increBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    input.value = parseInt(input.value) + 1;
 })
-
-/*Scroll to top when arrow up clicked*/
-$(window).scroll(function() {
-    var height = $(window).scrollTop();
-    if (height > 100) {
-        $('#back2Top').fadeIn();
-    } else {
-        $('#back2Top').fadeOut();
-    }
-});
-$(document).ready(function() {
-    $("#back2Top").click(function(event) {
-        event.preventDefault();
-        $("html, body").animate({ scrollTop: 0 }, "slow");
-        return false;
-    });
-
-});
-/* click menu ra */
-$(document).on('click', function() {
-    $('.collapse').collapse('hide');
-});
-// click menu-left ra
-/*$(".menu-btn").click(function() {
-    $(".menu-left").addClass("out");
-    return false;
-})
-// click menu-left vao
-$(".close-btn").click(function() {
-    $(".menu-left").removeClass("out");
-    return false;
-})
-$("body").click(function() {
-    $(".menu-left").removeClass("out");
-    return false;
-})*/
-// dropdown categories
+descBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    input.value = parseInt(input.value) - 1;
+}) 
