4b0daf74892e1087879272892cf9de71e9987fda
velocity5 <40029695+velocity5@users.noreply.github.com>
2020-07-22 22:51:08 +0700

    add media

--- front_end_09/zigtoys/js/index.js
+++ front_end_09/zigtoys/js/index.js
@@ -145,6 +145,24 @@ $(document).ready(function(){
 		}
 	});
   });
+  // increment function
+let increBtn = document.querySelector(".qty-incre");
+let descBtn = document.querySelector(".qty-desc");
+let input = document.querySelector("#qty");
+    increBtn.addEventListener('click', (e) => {
+        input.innerText = Number(input.innerText) + 1;
+        return false;
+    })
+    descBtn.addEventListener('click', (e) => {
+        input.innerText = Number(input.innerText);
+            if(input.innerText > 0) {
+                input.innerText = Number(input.innerText) - 1;
+            } if(input.innerText < 1) {
+                input.innerText = 1;
+            }
+            return false;
+    }) 
+
 // Add2cart function
 let carts = document.querySelectorAll(".add-cart");
 let products = [
