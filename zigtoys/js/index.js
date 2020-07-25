aeb5327f670da08c44265bf2b97728a15afa5af1
velocity5 <40029695+velocity5@users.noreply.github.com>
2020-07-25 09:30:54 +0700

    add widget

--- zigtoys/js/index.js
+++ zigtoys/js/index.js
@@ -380,3 +380,13 @@ displayCart();
 $(function() {
 	AOS.init();
 });
+/* live chat */
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5f1b88f35e51983a11f5c2f7/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
